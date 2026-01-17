8ed33a0c09d2ed29be8cbb787fe16583e019a54f
DoubleTea2 <nora.edwards223@gmail.com>
2026-01-17 15:19:56 -0600

    Init commit

---
+++ src/PracticeLambda.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PracticeLambda {
+    @Test
+    public void TestingLamda() {
+        assertEquals(1, 1);
+    }
+}
+
+
+
